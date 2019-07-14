ce0dbde7e405357b90f0c1ed84ed337d5784660c
Cornayy <cornegamer@live.nl>
2019-07-14 13:55:39 +0200

    Init

---
+++ server/schema/schema.js
@@ -0,0 +1,28 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {}
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
