4ba8d857515817d6a80b4da1b571bcbbfae07214
Cornayy <cornegamer@Live.nl>
2019-07-18 16:47:00 +0200

    Hidden connection string

--- server/app.js
+++ server/app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const config = require('./config/config');
 
 const app = express();
 
-mongoose.connect(
-    'mongodb+srv://Corne:pw1srspro@dev-cluster-bsmvv.mongodb.net/GraphQL?retryWrites=true&w=majority',
-    { useNewUrlParser: true }
-);
+mongoose.connect(config.connectionString, { useNewUrlParser: true });
 
 mongoose.connection.once('open', () => {
     console.log('Connected to database.');
